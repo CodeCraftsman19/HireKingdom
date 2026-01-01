081664d185ba9b4168e14556e9607d30ce64639c
Kaaditya19k <189aadi@gmail.com>
2026-01-01 13:02:07 +0530

    Fix CORS and API UR newto

--- server/server.js
+++ server/server.js
@@ -12,13 +12,21 @@ connectDB();
 const app = express();
 
 // Middleware
+// app.use(cors({
+//   origin: [
+//     "http://localhost:5173",
+//     "https://hirekindominternship.onrender.com"
+//   ],
+//   credentials: true
+// }));
+
 app.use(cors({
   origin: [
-    "http://localhost:5173",
-    "https://hirekindominternship.onrender.com"
-  ],
-  credentials: true
+    'http://localhost:3000',
+    'https://hire-kingdom-git-main-aaditya-kumars-projects-b2d1131e.vercel.app'
+  ]
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 

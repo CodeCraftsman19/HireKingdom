8c32b8ef8beae6f777ac0e4bf2b1df55d8157c1f
Kaaditya19k <189aadi@gmail.com>
2026-01-01 12:56:19 +0530

    Fix CORS and API UR newL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://hirekindominternship.onrender.com';
+const API_URL = 'https://hirekingdom.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
